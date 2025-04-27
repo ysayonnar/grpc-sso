4b0ebcd9d2db9cd6349f5763c02bc5db7f5b075d
ysayonnar <ysayonnar@gmail.com>
2025-04-27 20:33:21 +0300

    new: config and logger pkgs

---
+++ cmd/sso/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	//TODO: инициализировать логгер
+
+	//TODO: инициализировать приложение (app)
+
+	//TODO: запустить grpc приложение
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,19 @@ module sso
 
 go 1.24.2
 
-require github.com/ysayonnar/sso-protos v0.0.4 // indirect
+require (
+	github.com/fatih/color v1.18.0
+	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/ysayonnar/sso-protos v0.0.4 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
