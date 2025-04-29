62648d960dfa719ef41bed355346627189f0ba33
ysayonnar <ysayonnar@gmail.com>
2025-04-29 23:07:06 +0300

    fix: error duck typing fixed

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -17,6 +17,7 @@ import (
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 type Auth struct {
@@ -117,6 +118,11 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email string, password strin
 
 	id, err := a.userSaver.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
 		log.Error("failed to save user", sl.Err(err))
 
 		return 0, fmt.Errorf("%s: %w", op, err)
@@ -137,6 +143,12 @@ func (a *Auth) IsAdmin(ctx context.Context, userId int64) (bool, error) {
 
 	isAdmin, err := a.userProvider.IsAdmin(ctx, userId)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", sl.Err(err))
+
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+
+		}
 		return false, fmt.Errorf("%s: %w", op, err)
 	}
 
